f2cdcb47ae3f45d903b8bb66c0dbbafaa061f3c6
Rohit Pandilwar <160382472+R2932@users.noreply.github.com>
2025-09-16 17:53:15 +0530

    responisve cursor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -117,7 +117,7 @@ export default async function RootLayout({
           padding="0"
           horizontal="center"
         >
-          <div><SmoothCursor/></div>
+          <div className="sm:hidden"><SmoothCursor/></div>
           <RevealFx fill position="absolute">
             <Background
               mask={{
